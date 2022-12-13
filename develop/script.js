f0d91cbf8f3d635fa53076e5132be674de9ce4b3
Kaneknah <jamesnbaxley@protonmail.com>
2022-12-13 10:53:09 -0700

    Working API fetch for city

--- develop/script.js
+++ develop/script.js
@@ -3,14 +3,16 @@ var apiId = "bce1c0851022d2aa9806bc7732f1bf27"
 var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
 var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="
 var units = "units=imperial"
-var searchText = $("#Search-input").val();
 var savedSearches = ""
 var neededParams = "&APPID=" + apiId + "&" + units;
 
 
 //Start Search function: Connects the button and search ID to run the other functions
-$(document).ready(function() {
-    $("#search-button").on("click", function() {
+$(document).ready(function(event) {
+    var searchText = $("#Search-input").val();
+
+    $("#search-button").on("click", function(event) {
+    event.preventDefault();
 
 searchCurrentWeather(searchText);
 searchFiveDay(searchText);
@@ -22,7 +24,9 @@ $("#Search-input").val("");
 
 //function for current Weather API Fetch
 function searchCurrentWeather() {
+    var searchText = $("#Search-input").val();
 var url = currentWeather + searchText + neededParams;
+console.log(searchText)
 console.log(url)
 
 fetch(url) 
@@ -32,6 +36,7 @@ fetch(url)
 
 //function for 5day forecast API Fetch
 function searchFiveDay(){
+    var searchText = $("#Search-input").val();
     var url = fiveDayForecast + searchText + neededParams;
     console.log(url)
     
