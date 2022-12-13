efb2a62c46d005c17a220c8980c5f421371e4f29
Kaneknah <jamesnbaxley@protonmail.com>
2022-12-13 09:33:09 -0700

    JS jQuery for search function and 5 day/current

--- develop/script.js
+++ develop/script.js
@@ -3,59 +3,47 @@ var apiId = "bce1c0851022d2aa9806bc7732f1bf27"
 var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
 var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="
 var units = "units=imperial"
-var searchText = ("id", "search-input").val
+var searchText = $("#Search-input").val();
 var savedCities = ""
 var neededParams = "&APPID=" + apiId + "&" + units;
 
 
-// function startSearch(event) {
-//     event.preventDefault();
+//Start Search function: Connects the button and search ID to run the other functions
+$(document).ready(function() {
+    $("#search-button").on("click", function() {
 
-//     }
-// //add event listener to button
-// if statement for blank search or not readable search
-//API search code
+searchCurrentWeather(searchText);
+searchFiveDay(searchText);
 
+// $("#Search-input").val("");
+
+    })
+})
+
+//function for current Weather
+function searchCurrentWeather() {
+var url = currentWeather + searchText + neededParams;
+console.log(url)
+
+fetch(url) 
+.then(response => response.json()
+.then(data => console.log(data)));
+}
 
 //finding a 5 day forecast for input city
-function searchFiveDay(query){
-    var url = fiveDayForecast + "Missoula" + neededParams;
-    console.log(url)
-    console.log("hi")
-    fetch(url)
-}
-searchFiveDay()
+// function searchFiveDay(){
+//     var url = fiveDayForecast + searchText + neededParams;
+//     console.log(url)
+    
+//     fetch(url)
+//     .then(response => response.json()
+//     .then(data => console.log(data)));
+// }
+
+
+
 //     .then(function(response) {
 //         if(response.ok) {
 //              return response.json().then(function(data) {
 //                 console.log(url)})}
-// //API search for 5 day data
-
-
-// .then((response) =>{
-//     if (response.ok) {
-//         return response.json();
-//     }
-//     else{
-//         throw new Error("NetWork Response Error");
-//     }
-// })
-// .then(data => {
-//     console.log(data)
-//     displayWeather(data)
-// })
-// .catch((error)=> console.error("Fetch error:", error));
-
-// function renderCurrentWeather(){
-// //API search for current data
-
-// }
-
-// function renderFiveDay(){
-
-// }
-
-// function savedSearches(){
-
-// }
 
