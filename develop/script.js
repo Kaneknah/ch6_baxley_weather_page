336f3e48c75448970e7244d5b4ccca4bfd8ae8de
Kaneknah <jamesnbaxley@protonmail.com>
2022-12-15 12:22:50 -0700

    Working on Render function with data

--- develop/script.js
+++ develop/script.js
@@ -1,15 +1,24 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
 
-var apiId = "bce1c0851022d2aa9806bc7732f1bf27"
-var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
-var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="
-var units = "units=imperial"
-var savedSearches = ""
-var neededParams = "&APPID=" + apiId + "&" + units;
+const apiId = "bce1c0851022d2aa9806bc7732f1bf27"
+const currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
+const fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q="
+const units = "units=imperial"
+let savedSearches = ""
+const neededParams = "&APPID=" + apiId + "&" + units;
 
 
 //Start Search function: Connects the button and search ID to run the other functions
 $(document).ready(function(event) {
-    var searchText = $("#Search-input").val();
+    let searchText = $("#Search-input").val();
 
     $("#search-button").on("click", function(event) {
     event.preventDefault();
@@ -24,27 +33,60 @@ $("#Search-input").val("");
 
 //function for current Weather API Fetch
 function searchCurrentWeather() {
-    var searchText = $("#Search-input").val();
-var url = currentWeather + searchText + neededParams;
+    let searchText = $("#Search-input").val();
+    let url = currentWeather + searchText + neededParams;
+
 console.log(searchText)
 console.log(url)
 
-fetch(url) 
-.then(response => response.json()
-.then(data => console.log(data)));
+    fetch(url) 
+        .then(response => {
+            return response.json();
+        })
+        .then((data) => {
+            let apiData = data;
+        })
+console.log(data);
+       
+
 }
 
 //function for 5day forecast API Fetch
-function searchFiveDay(){
-    var searchText = $("#Search-input").val();
-    var url = fiveDayForecast + searchText + neededParams;
-    console.log(url)
-    
-    fetch(url)
-    .then(response => response.json()
-    .then(data => console.log(data)));
-}
+// function searchFiveDay(){
+//     let searchText = $("#Search-input").val();
+//     let url = fiveDayForecast + searchText + neededParams;
 
+//     console.log(url)
+    
+//     fetch(url)
+//         .then(response => response.json()
+//         .then(data => console.log(data)));
+
+//         renderFiveDay(data);
+// }
+
+function renderCurrentWeather() {
+ let location = document.querySelector(".name-title");
+ let name = data.name;
+ location.textContent = name
+
+ let currentDate = document.querySelector(".date");
+ let date = moment().format();
+currentDate.textContent = date;
+
+ let currentTemp = document.querySelector(".temp");
+ let temp = data.main.temp;
+ currentTemp.textContent = "Temperature: " + temp + "F";
+
+ let currentHumidity = document.querySelector(".humidity");
+ let humidity = data.main.humidity;
+ currentHumidity.textContent = "Humidity: " + humidity
+
+
+  console.log(data);
+    console.log(location);
+    console.log(temp);
+}
 
 //  If statements for guarding against failed API fetches?
 //     .then(function(response) {
