153aa6e33e38fb9a7c6190cf3095e53f08847b51
Kaneknah <jamesnbaxley@protonmail.com>
2022-12-28 12:53:34 -0700

    5 day forcast updated

--- develop/script.js
+++ develop/script.js
@@ -19,96 +19,134 @@ const neededParams = "&APPID=" + apiId + "&units=imperial";
 let cityData = document.querySelector(".city-data");
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
 let dataList = document.querySelector(".data-list");
+let fiveDayCardParent = document.querySelector(".five-day-forecast")
+let searchedContainer = document.querySelector(".searched-container")
 
-//Start Search function: Connects the button and search ID to run the other functions
+//Start Search function: Connects the button and search ID to run the other functions.
 $(document).ready(function (event) {
   $("#search-button").on("click", function (event) {
     event.preventDefault();
 
     let searchText = $("#Search-input").val();
-    fetchCurrentWeather(searchText);
+    fetchWeather(searchText);
   });
 });
 
-//function for current Weather API Fetch
-async function fetchCurrentWeather(searchText) {
-  let url = currentWeather + searchText + neededParams;
-  let response = await fetch(url);
-  renderCurrentWeather(await response.json());
+//function for current Weather API Fetch.
+async function fetchWeather(searchText) {
+  let currentWeatherUrl = currentWeather + searchText + neededParams;
+  let fiveDayWeatherUrl = fiveDayForecast + searchText + neededParams;
+
+  let currentWeatherResponse = await fetch(currentWeatherUrl);
+  renderWeather(await currentWeatherResponse.json());
+
+  let fiveDayWeatherResponse = await fetch(fiveDayWeatherUrl);
+  renderWeather(await fiveDayWeatherResponse.json());
+
+//   if (data === Object){
+//     for (let i = 1; i < 6; i++){
+//       renderWeather(i)
+//     }
+//   }
+//   else {
+//     renderWeather()
+
+//   }
+// }
+}
+function renderWeather(data) {
+
+if (data["list"]) {
+   console.log(data)
+  for (let i = 1; i < 40; i+=8){
+    let fiveDayCard = document.createElement("div")
+    fiveDayCard.setAttribute("class", "five-day-card")
+    fiveDayCardParent.appendChild("fiveDayCard")
+   
+    currentData = data["list"][i]
+    processData(currentData, fiveDayCard)
+    //create box for single day results
+  }
+} else {
+  let singleDayCard = document.createElement("div")
+  singleDayCard = singleDayCard.setAttribute("class", "single-day-card")
+  searchedContainer.append("singleDayCard")
+  processData(data, singleDayCard)
+   
 }
 
-function renderCurrentWeather(data) {
-  console.log(data);
 
-//Creation of current weather location name
-  let location = document.createElement("h2");
+ 
+}
+
+function processData(data, cardEl){
+console.log(cardEl)
+  //Creation of current weather location name.
+   let location = document.createElement("h2");
   location.setAttribute("class", "return-name")
   let name = data.name;
   location.textContent = name;
 
-  //Creation of current weather date
-  let currentDate = document.createElement("h2");
-  let date = moment().format("MM/DD/YYYY");
-  currentDate.setAttribute("class", "current-date")
-  currentDate.textContent = date;
+  //Creation of current weather date.
+  let currentDateEl = document.createElement("h2");
+  let date = data.dt_txt ? data.dt_txt : moment().format("MM/DD/YYYY");
+  currentDateEl.setAttribute("class", "current-date")
+  currentDateEl.textContent = date;
 
   //Creation of current weather icon
-  let currentIcon = document.createElement("img")
-  currentIcon.setAttribute("class", "current-icon")
-  let icon = data.weather[0].icon;
-  currentIcon.src =  weatherIconUrl + icon + ".png";
-  let displayIcon = currentIcon.val
-  // currentIcon.textContent = weatherIconUrl + icon + ".png";
-  
-  console.log(icon)
-  console.log(currentIcon)
+  let currentIconEl = document.createElement("img");
+  // http://openweathermap.org/img/wn/01d.png
+  currentIconEl.setAttribute("class", "current-icon");
+  let icon = weatherIconUrl + data.weather[0].icon + ".png";
+  currentIconEl.src = icon;
 
-  cityData.append(name + " " + date + " " + displayIcon)
+  cardEl.append(name + " " + date)
+  cardEl.append(currentIconEl)
 
-  //creation of current weather temp
-  let currentTemp = document.createElement("p");
-  currentTemp.setAttribute("class", "return-temp")
+  //creation of current weather temp.
+  let currentTempEl = document.createElement("p");
+  currentTempEl.setAttribute("class", "return-temp")
   let temp = data.main.temp;
-  currentTemp.textContent = "Temperature: " + temp + "F";
+  currentTempEl.textContent = "Temperature: " + temp + "F";
 
-  dataList.appendChild(currentTemp);
+  cardEl.appendChild(currentTempEl);
 
-  //creation of current weather humidity
-  let currentHumidity = document.createElement("p");
-  currentHumidity.setAttribute("class", "return-humidity");
+  //creation of current weather humidity.
+  let currentHumidityEl = document.createElement("p");
+  currentHumidityEl.setAttribute("class", "return-humidity");
   let humidity = data.main.humidity;
-  currentHumidity.textContent =  "Humidity: " + humidity;
+  currentHumidityEl.textContent = "Humidity: " + humidity + " %";
 
-dataList.appendChild(humidity);
+cardEl.appendChild(currentHumidityEl);
 
-//creation of current weather wind speed
-  let currentWindSpeed = document.createElement("p");
-  currentWindSpeed.setAttribute("class", "return-windspeed");
-  let windSpeed = data.main.wind_speed;
-  currentWindSpeed.textContent =  "Wind Speed: " + windSpeed + " MPH";
+//creation of current weather wind speed.
+  let currentWindSpeedEl = document.createElement("p");
+  currentWindSpeedEl.setAttribute("class", "return-wind-speed");
+  let windSpeed = data.wind.speed;
+  currentWindSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
 
-dataList.appendChild(windSpeed);
-
-  console.log(location);
-  console.log(humidity);
-  console.log(temp);
+cardEl.appendChild(currentWindSpeedEl);
+ console.log(data)
 }
 
+
 //Tested Code
-
-
-//function for 5day forecast API Fetch
-// function searchFiveDay(){
-//     let searchText = $("#Search-input").val();
-//     let url = fiveDayForecast + searchText + neededParams;
-
+// function for 5day forecast API Fetch
+// async function fetchFiveDayWeather(searchText){
+    
 //     console.log(url)
 
 //     fetch(url)
 //         .then(response => response.json()
 //         .then(data => console.log(data)));
 
-//         renderFiveDay(data);
+        // renderFiveDay(data);
+// }
+//function for current Weather API Fetch.
+// async function fetchCurrentWeather(searchText) {
+//   let url = currentWeather + searchText + neededParams;
+//   let response = await fetch(url);
+//   renderCurrentWeather(await response.json());
 // }
 
 
