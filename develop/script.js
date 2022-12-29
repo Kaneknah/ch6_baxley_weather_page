5b47534177d94ece2c16bfb010a12fe45875408f
Kaneknah <jamesnbaxley@protonmail.com>
2022-12-29 14:45:04 -0700

    working single and 5 day api call

--- develop/script.js
+++ develop/script.js
@@ -10,23 +10,20 @@
 //the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-
+let searchResults = document.querySelector(".search-results");
 const apiId = "bce1c0851022d2aa9806bc7732f1bf27";
 const currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=";
 const fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=";
-let savedSearches = "";
 const neededParams = "&APPID=" + apiId + "&units=imperial";
-let cityData = document.querySelector(".city-data");
 const weatherIconUrl = 'http://openweathermap.org/img/wn/';
-let dataList = document.querySelector(".data-list");
-let fiveDayCardParent = document.querySelector(".five-day-forecast")
-let searchedContainer = document.querySelector(".searched-container")
+let fiveDayCardParent = document.querySelector(".five-day-forecast");
+let searchedContainer = document.querySelector(".searched-container");
 
 //Start Search function: Connects the button and search ID to run the other functions.
 $(document).ready(function (event) {
   $("#search-button").on("click", function (event) {
     event.preventDefault();
-
+    searchResults.remove();
     let searchText = $("#Search-input").val();
     fetchWeather(searchText);
   });
@@ -43,54 +40,50 @@ async function fetchWeather(searchText) {
   let fiveDayWeatherResponse = await fetch(fiveDayWeatherUrl);
   renderWeather(await fiveDayWeatherResponse.json());
 
-//   if (data === Object){
-//     for (let i = 1; i < 6; i++){
-//       renderWeather(i)
-//     }
-//   }
-//   else {
-//     renderWeather()
-
-//   }
-// }
 }
 function renderWeather(data) {
 
 if (data["list"]) {
-   console.log(data)
+   console.log(data);
   for (let i = 1; i < 40; i+=8){
-    let fiveDayCard = document.createElement("div")
-    fiveDayCard.setAttribute("class", "five-day-card")
-    fiveDayCardParent.appendChild("fiveDayCard")
-   
-    currentData = data["list"][i]
-    processData(currentData, fiveDayCard)
+    let fiveDayCard = document.createElement("div");
+    fiveDayCard.setAttribute("class", "five-day-card");
+    fiveDayCardParent.appendChild(fiveDayCard);
+    currentData = data["list"][i];
+    processData(currentData, fiveDayCard);
     //create box for single day results
   }
 } else {
-  let singleDayCard = document.createElement("div")
-  singleDayCard = singleDayCard.setAttribute("class", "single-day-card")
-  searchedContainer.append("singleDayCard")
-  processData(data, singleDayCard)
-   
+  let singleDayCard = document.createElement("div");
+  singleDayCard.setAttribute("class", "single-day-card");
+  searchedContainer.append(singleDayCard);
+  processData(data, singleDayCard);
 }
-
-
  
 }
-
 function processData(data, cardEl){
-console.log(cardEl)
+
+  //creating elements for HTML
+  let cityData = document.createElement("div");
+  cardEl.appendChild(cityData);
+  cityData.setAttribute("class", "city-data");
+
+  let weatherData = document.createElement("div");
+  cardEl.appendChild(weatherData);
+  weatherData.setAttribute("Class", "weather-data");
+
   //Creation of current weather location name.
    let location = document.createElement("h2");
-  location.setAttribute("class", "return-name")
-  let name = data.name;
+  location.setAttribute("class", "return-name");
+  let name = data.name ? data.name : "";
   location.textContent = name;
 
   //Creation of current weather date.
   let currentDateEl = document.createElement("h2");
   let date = data.dt_txt ? data.dt_txt : moment().format("MM/DD/YYYY");
-  currentDateEl.setAttribute("class", "current-date")
+  let whiteSpaceIndex = date.indexOf(" ");
+  date = date.slice(0, whiteSpaceIndex);
+  currentDateEl.setAttribute("class", "current-date");
   currentDateEl.textContent = date;
 
   //Creation of current weather icon
@@ -100,16 +93,16 @@ console.log(cardEl)
   let icon = weatherIconUrl + data.weather[0].icon + ".png";
   currentIconEl.src = icon;
 
-  cardEl.append(name + " " + date)
-  cardEl.append(currentIconEl)
+  cityData.append(name + " " + date);
+  cityData.append(currentIconEl);
 
   //creation of current weather temp.
   let currentTempEl = document.createElement("p");
-  currentTempEl.setAttribute("class", "return-temp")
+  currentTempEl.setAttribute("class", "return-temp");
   let temp = data.main.temp;
   currentTempEl.textContent = "Temperature: " + temp + "F";
 
-  cardEl.appendChild(currentTempEl);
+  weatherData.appendChild(currentTempEl);
 
   //creation of current weather humidity.
   let currentHumidityEl = document.createElement("p");
@@ -117,7 +110,7 @@ console.log(cardEl)
   let humidity = data.main.humidity;
   currentHumidityEl.textContent = "Humidity: " + humidity + " %";
 
-cardEl.appendChild(currentHumidityEl);
+weatherData.appendChild(currentHumidityEl);
 
 //creation of current weather wind speed.
   let currentWindSpeedEl = document.createElement("p");
@@ -125,8 +118,8 @@ cardEl.appendChild(currentHumidityEl);
   let windSpeed = data.wind.speed;
   currentWindSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
 
-cardEl.appendChild(currentWindSpeedEl);
- console.log(data)
+weatherData.appendChild(currentWindSpeedEl);
+ console.log(data);
 }
 
 
